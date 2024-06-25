3e6a6f3d9c6b62294af85c7dbec36188f23a4ff4
PR23107147-janindu <janidulakshan08@gmail.com>
2024-06-25 14:26:49 +0530

    Update

--- src/main/java/org/example/edu/controllers/SigninPageController.java
+++ src/main/java/org/example/edu/controllers/SigninPageController.java
@@ -14,59 +14,54 @@ import org.example.edu.util.BoType;
 import java.io.IOException;
 
 public class SigninPageController {
-    public AnchorPane welcomeAnchor;
+    
     public JFXTextField username;
-
     public JFXPasswordField passwordfield;
+    public AnchorPane signinanchor;
 
 
     SceneSwitchController sceneSwitch = SceneSwitchController.getInstance();
-    EmployeeBoImpl userBoImpl=new EmployeeBoImpl();
+    EmployeeBoImpl employeeBoImpl = new EmployeeBoImpl();
 
-
-
-    public void forgetAction(ActionEvent actionEvent) throws IOException {
-        sceneSwitch.switchScene(welcomeAnchor, "ResetForm.fxml");
-    }
-
-    public void NeedAction(ActionEvent actionEvent) {
-
-    }
 
     public void signinaction(ActionEvent actionEvent) throws IOException {
 
-        userBoImpl=BoFactory.getInstance().getBo(BoType.USER);
+        employeeBoImpl = BoFactory.getInstance().getBo(BoType.USER);
 
-        UserEntity userEntity = userBoImpl.getUserByEmail(username.getText());
+        UserEntity userEntity = employeeBoImpl.getUserByEmail(username.getText());
 
 
-        if (userEntity==null){
-            new Alert(Alert.AlertType.ERROR,"Invalid Email Address or Password").show();
+        if (userEntity == null) {
+            new Alert(Alert.AlertType.ERROR, "Invalid Email Address or Password").show();
             return;
         }
 
-        String password = userBoImpl.passwordDecrypt(userEntity.getPassword());
+        String password = employeeBoImpl.passwordDecrypt(userEntity.getPassword());
 
-        if (userEntity.getRole().equals("Admin Panel") && password.equals(passwordfield.getText())){
+        if (userEntity.getRole().equals("Admin Panel") && password.equals(passwordfield.getText())) {
             System.out.println("Logged");
             try {
-                SceneSwitchController.getInstance().switchScene(welcomeAnchor, "UserRegisterForm.fxml");
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+                SceneSwitchController.getInstance().switchScene(signinanchor, "manageEmployee-form.fxml");
+            } catch (IOException i) {
+                throw new RuntimeException(i);
             }
-        }else if (userEntity.getRole().equals("Employee") && password.equals(passwordfield.getText())){
-            SceneSwitchController.getInstance().switchScene(welcomeAnchor, "AddCusFormController.fxml");
-        } else if (userEntity.getId()==null) {
+        } else if (userEntity.getRole().equals("Employee") && password.equals(passwordfield.getText())) {
+            SceneSwitchController.getInstance().switchScene(signinanchor, "manageCustomers-form.fxml");
+        } else if (userEntity.getId() == null) {
             System.out.println("Null");
-        } else{
-            new Alert(Alert.AlertType.ERROR,"Invalid Password").show();
+        } else {
+            new Alert(Alert.AlertType.ERROR, "Invalid Password").show();
         }
 
     }
 
-    public void link(MouseEvent mouseEvent) {
 
 
+    public void forgetaction(ActionEvent actionEvent) throws IOException {
+        sceneSwitch.switchScene(signinanchor, "landingPage-form.fxml");
+    }
+
+    public void needaction(ActionEvent actionEvent) {
     }
 }
 
