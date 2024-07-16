537df2e9bb5a6d6cd30c70a98daf22551fac11bb
PR23107147-janindu <janidulakshan08@gmail.com>
2024-07-16 12:50:08 +0530

    Update

--- src/main/java/org/example/edu/controllers/ViewProductsController.java
+++ src/main/java/org/example/edu/controllers/ViewProductsController.java
@@ -1,7 +1,106 @@
 package org.example.edu.controllers;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import org.example.edu.bo.custom.impl.ProductBoImpl;
+import org.example.edu.model.Product;
 
-public class ViewProductsController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ViewProductsController implements Initializable {
     public AnchorPane viewOrderanchor;
+    public AnchorPane productanchor;
+    public JFXTextField pId;
+    public JFXTextField pName;
+    public JFXTextField pQty;
+    public TableView productTable;
+    public TableColumn idcol;
+    public TableColumn namecol;
+    public TableColumn sizecol;
+    public TableColumn qtycol;
+    public TableColumn supIdcol;
+    public TableColumn pricePerOnecol;
+    public ComboBox cmbSize;
+    public ComboBox cmbSup;
+    public JFXTextField pricePerOne;
+
+    ProductBoImpl productBoImpl = new ProductBoImpl();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        idcol.setCellValueFactory(new PropertyValueFactory<>("id"));
+        namecol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        sizecol.setCellValueFactory(new PropertyValueFactory<>("size"));
+        qtycol.setCellValueFactory(new PropertyValueFactory<>("qty"));
+        supIdcol.setCellValueFactory(new PropertyValueFactory<>("supId"));
+        pricePerOnecol.setCellValueFactory(new PropertyValueFactory<>("price"));
+
+        productTable.setItems(productBoImpl.getAllPro());
+
+    }
+    SceneSwitchController sceneSwitch = SceneSwitchController.getInstance();
+
+    public void AnchorClick(MouseEvent mouseEvent) {
+
+        pId.setText("");
+        pName.setText("");
+        pQty.setText(String.valueOf(""));
+        pricePerOne.setText(String.valueOf(""));
+        cmbSize.getSelectionModel().select("");
+        cmbSup.getSelectionModel().select("");
+    }
+
+
+
+    public void ProductAction(ActionEvent actionEvent) {
+    }
+
+    public void CusAction(ActionEvent actionEvent) {
+    }
+
+    public void PlaceAction(ActionEvent actionEvent) {
+    }
+
+    public void SupplierAction(ActionEvent actionEvent) {
+    }
+
+    public void log(ActionEvent actionEvent) {
+    }
+
+    boolean isAction = true,isMouseClick,isPriceValid ;
+    public void dataBar(MouseEvent mouseEvent) {
+        int index = productTable.getSelectionModel().getSelectedIndex();
+
+
+        if(index < 0){
+            return;
+        }
+        String id = idcol.getCellData(index).toString();
+
+        if (isAction){
+            isPriceValid = true;
+            Product product = productBoImpl.getProById(id);
+            pId.setText(product.getId());
+            pName.setText(product.getName());
+            pQty.setText(String.valueOf(product.getQty()));
+            pricePerOne.setText(String.valueOf(product.getPrice()));
+            cmbSize.getSelectionModel().select(product.getSize());
+            cmbSup.getSelectionModel().select(product.getSupId());
+
+            if (!product.getId().equals("")){
+                isMouseClick = true;
+            }
+        }
+    }
+
+
 }
